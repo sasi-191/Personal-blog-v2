6c2c20b662d9f07874b7519061653ea5e7440c54
sasi-191 <bsasikarthik191@gmail.com>
2024-04-02 10:55:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function show(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+   }
+
+   function hide(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "none";
+   }
+
+
